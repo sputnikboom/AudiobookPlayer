e00eb47b693119f59b267da8467449e859e04afe
Rachael Hall <rachael.hall.889@gmail.com>
2018-04-27 21:56:10 +0100

    added onClickListeners to MainActivity and NowPlaying, amended layout for NowPlaying

--- app/src/main/java/com/example/rach/audiobookplayer/NowPlayingActivity.java
+++ app/src/main/java/com/example/rach/audiobookplayer/NowPlayingActivity.java
@@ -1,7 +1,11 @@
 package com.example.rach.audiobookplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NowPlayingActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class NowPlayingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+
+
+        // checks for clicks on the home button
+        Button homeButton = (Button) findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent homeButton = new Intent(NowPlayingActivity.this, MainActivity.class);
+                startActivity(homeButton);
+            }
+        });
     }
+
 }
